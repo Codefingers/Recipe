e171f1fac5b34961d1d9e1719fa6f983e027cea0
Ernest Lukos <ernest-lukos@hotmail.co.uk>
2020-06-15 22:55:09 +0100

    update laravel

---
+++ app/Ingredient.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    public function recipe()
+    {
+//        return $this->hasMany()
+    }
+}
---
+++ app/Recipe.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    public function ingredient()
+    {
+        return $this->hasMany('App\Ingredient');
+    }
+}
