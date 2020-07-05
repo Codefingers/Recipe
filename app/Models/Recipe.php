377b27a1639d051021bd17b2069ca5cb570f31e1
Ernest Lukos <ernest-lukos@hotmail.co.uk>
2020-07-05 18:11:03 +0100

    update test coverage and adhere to phpstan

---
+++ app/Models/Recipe.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * Class Recipe
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Ingredient[] $ingredients
+ * @property-read int|null $ingredients_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Recipe newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Recipe newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Recipe query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Recipe whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Recipe whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Recipe whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Recipe whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Recipe extends Model
+{
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class);
+    }
+}
