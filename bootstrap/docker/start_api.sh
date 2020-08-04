abbfb00d80d94e8d0998356d10a61b0ed6122f92
Ernest Lukos <elukos@workforcesoftware.com>
2020-08-04 22:07:56 +0100

    CICD

--- bootstrap/docker/start_api.sh
+++ bootstrap/docker/start_api.sh
@@ -5,6 +5,14 @@ curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
 
 # Install all PHP dependencies.
 cd /app/
+
+# Install unzip utility and libs needed by zip PHP extension
+apt-get update && apt-get install -y \
+    zlib1g-dev \
+    libzip-dev \
+    unzip
+docker-php-ext-install zip
+
 composer install
 
 cp .env.example .env
