8d5ef4b57869f851b03e431c7175962198d0d49f
Ernest Lukos <elukos@workforcesoftware.com>
2020-08-08 14:58:43 +0100

    patch

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
-FROM docker.pkg.github.com/codefingers/recipe/recipe_api:1
+FROM php:7.4-cli
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install all PHP dependencies.
+RUN cd /app/
+
+# Install unzip utility and libs needed by zip PHP extension
+RUN apt-get update && apt-get install -y \
+    zlib1g-dev \
+    libzip-dev \
+    unzip
+docker-php-ext-install zip
+
+RUN composer install
--- bootstrap/docker/start_api.sh
+++ bootstrap/docker/start_api.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -e
 
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-# Install all PHP dependencies.
-cd /app/
-
-# Install unzip utility and libs needed by zip PHP extension
-apt-get update && apt-get install -y \
-    zlib1g-dev \
-    libzip-dev \
-    unzip
-docker-php-ext-install zip
-
-composer install
-
 cp .env.example .env
 php artisan key:generate --no-interaction
 php artisan jwt:secret --no-interaction
