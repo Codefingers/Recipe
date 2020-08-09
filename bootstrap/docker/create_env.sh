dc39b2383cf1522f56101a7db1d766b61faae8e8
Ernest Lukos <elukos@workforcesoftware.com>
2020-08-09 13:38:27 +0100

    uzdfjdigj

--- bootstrap/docker/create_env.sh
+++ bootstrap/docker/create_env.sh
@@ -2,6 +2,10 @@
 set -e
 
 cp .env.example .env
+
+echo ${1}
+echo $1
+
 sed "s/DB_HOST=/DB_HOST=${1}/" .env
 sed "s/DB_DATABASE=/DB_DATABASE=${2}/" .env
 sed "s/DB_USERNAME=/DB_USERNAME=${3}/" .env
