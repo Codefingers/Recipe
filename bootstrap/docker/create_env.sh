af6d2e4196e7dd230932d3a68d872732ed729662
Ernest Lukos <elukos@workforcesoftware.com>
2020-08-09 13:58:01 +0100

    test again last

--- bootstrap/docker/create_env.sh
+++ bootstrap/docker/create_env.sh
@@ -3,10 +3,9 @@ set -e
 
 cp .env.example .env
 
-echo ${1}
 echo $1
 
-sed "s/DB_HOST=/DB_HOST=${1}/" .env
-sed "s/DB_DATABASE=/DB_DATABASE=${2}/" .env
-sed "s/DB_USERNAME=/DB_USERNAME=${3}/" .env
-sed "s/DB_PASSWORD=/DB_PASSWORD=${4}/" .env
+sed -n -i "s/DB_HOST=/DB_HOST=${1}/" .env
+sed -n -i "s/DB_DATABASE=/DB_DATABASE=${2}/" .env
+sed -n -i "s/DB_USERNAME=/DB_USERNAME=${3}/" .env
+sed -n -i "s/DB_PASSWORD=/DB_PASSWORD=${4}/" .env
