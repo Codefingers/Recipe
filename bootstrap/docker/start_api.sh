2458fd9ed55ce82816595e24c86d704d7c6b66ee
Ernest Lukos <elukos@workforcesoftware.com>
2020-08-09 14:57:29 +0100

    update shell

--- bootstrap/docker/start_api.sh
+++ bootstrap/docker/start_api.sh
@@ -3,7 +3,7 @@ set -e
 
 php artisan key:generate --no-interaction
 php artisan jwt:secret --no-interaction
-php artisan migrate exit
+php artisan migrate
 
 echo "Starting web-server..."
 exec php artisan serve --host=0.0.0.0 --port=80
